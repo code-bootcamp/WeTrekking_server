b862d55b12fb0af54c8ce68a57ee1540f54b8129
seogoo12 <seogoo12@naver.com>
2022-10-31 22:20:51 +0900

    [Feat] crewBoasds API 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CrewBoardModule } from './apis/crewBoards/crewBoard.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CrewBoardModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graghql/schema.gql',
+      context: ({ req, res }) => ({ req, res }),
+    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
 })
 export class AppModule {}
